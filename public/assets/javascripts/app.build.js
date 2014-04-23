8b60071180723871046cedc8f7d061cf02d7d871
Kevin Colten <kevincolten@gmail.com>
2014-04-23 00:33:51 -0500

    r.js setup

---
+++ public/assets/javascripts/app.build.js
@@ -0,0 +1,18 @@
+({
+    dir: "../js-optimized",
+
+
+    paths: {
+            "jquery": "/assets/javascripts/jquery/jquery",
+            "underscore": "/assets/javascripts/underscore/underscore",
+            "backbone": "/assets/javascripts/backbone/backbone",
+            "jquery-mobile": "/assets/javascripts/jquery-mobile-bower/js/jquery.mobile-1.4.2.min",
+            "jackbone": "/assets/javascripts/jackbone/jackbone",
+            "tpl": "/assets/javascripts/requirejs-tpl/tpl",
+            "backbone-query-parameters": "/assets/javascripts/backbone-query-parameters/backbone.queryparams",
+            "backbone-query-parameters-shim": "/assets/javascripts/backbone-query-parameters/backbone.queryparams-1.1-shim",
+            "serializeJSON": "/assets/javascripts/jquery.serializeJSON/jquery.serializejson",
+            "jqm-config": "/assets/javascripts/jqm-config",
+            "fastclick": "/assets/javascripts/fastclick/lib/fastclick"
+        }
+    })
