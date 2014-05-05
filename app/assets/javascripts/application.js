272448ff815a39d5c198d0e01f8036fc5f199ee8
Kevin Colten <kevincolten@gmail.com>
2014-05-04 23:27:22 -0500

    working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,4 @@
-require.config({
-    paths: {
-        "jquery": "/assets/javascripts/jquery/jquery",
-        "underscore": "/assets/javascripts/underscore/underscore",
-        "backbone": "/assets/javascripts/backbone/backbone",
-        "jquery-mobile": "/assets/javascripts/jquery-mobile-bower/js/jquery.mobile-1.4.2.min",
-        "jackbone": "/assets/javascripts/jackbone/jackbone",
-        "tpl": "/assets/javascripts/requirejs-tpl/tpl",
-        "backbone-query-parameters": "/assets/javascripts/backbone-query-parameters/backbone.queryparams",
-        "backbone-query-parameters-shim": "/assets/javascripts/backbone-query-parameters/backbone.queryparams-1.1-shim",
-        "serializeJSON": "/assets/javascripts/jquery.serializeJSON/jquery.serializejson",
-        "jqm-config": "/assets/javascripts/jqm-config",
-        "fastclick": "/assets/javascripts/fastclick/lib/fastclick"
-    },
-
-    shim: {
-        'backbone-query-parameters-shim': ['backbone'],
-        'jqm-config': ["jquery-mobile"]
-    }
-});
-
-define(["./router", "jquery-mobile", "jqm-config", "fastclick"], 
-
+define(["./router", "jquery-mobile", "jqm-config", "fastclick"],
 function(Router) {
     
     window.addEventListener('load', function() {
